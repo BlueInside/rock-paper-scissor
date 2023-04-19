f801c85bf11d03ecf75d0fc41bf86f58a0c7bdea
Karol <kpulawski13@gmail.com>
2023-04-19 11:58:27 +0100

    create playerSelect and computerSelect functions     also make function that plays one round

---
+++ index.js
@@ -0,0 +1,56 @@
+// 1 We will play against computer that randomly return Rock paper or scissor
+//   - function that returns "Rock" "Paper" or "Scissor" randomly
+function computerSelection() {
+  let randomNum = Math.floor(Math.random() * 3) + 1;
+  let result = "";
+  if (randomNum === 1) result = "Rock";
+  else if (randomNum === 2) result = "Paper";
+  else result = "Scissor";
+  return result.toLowerCase();
+}
+//player selection function return paper scissor or rock
+function playerSelection() {
+  let userInput = prompt("Choose rock paper or scissor").toLowerCase();
+  if (
+    userInput !== "rock" &&
+    userInput !== "paper" &&
+    userInput !== "scissor"
+  ) {
+    alert("Please type rock paper or scissor!");
+    //repeats if input was incorrect
+    return playerSelection();
+  } else {
+    return userInput;
+  }
+}
+
+// 2 Write function that plays one round of Rock Paper Scissors
+// function take two params playerSelection adn computerSelection returns a string that declares winner of round
+//    - function that takes two args (Rock paper scissor) compares them according to RPS rules and return winner player or computer
+function playRound(computerSelection, playerSelection) {
+  let playerChoice = playerSelection();
+  let computerChoice = computerSelection();
+  // log to the user choices
+  console.log(`
+  Player chose:     ${playerChoice}
+  Computer chose:   ${computerChoice}
+  `);
+  let winner = "";
+  // logic that compares players choices
+  // when computer wins
+  if (
+    (computerChoice === "rock" && playerChoice === "scissor") ||
+    (computerChoice === "paper" && playerChoice === "rock") ||
+    (computerChoice === "scissor" && playerChoice === "paper")
+  ) {
+    return "Computer win";
+  } else if (computerChoice === playerChoice) return "Draw";
+  else return "Player win!";
+}
+//   make function playerSelection case-insensitive
+// 3 Write new function game()
+// use it to play 5 round game that keeps score and reports winner
+// use console.log() to display the results of each round and winner
+// use prompt to get input from user
+
+console.log(playRound(computerSelection, playerSelection));
